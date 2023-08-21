ec1da2fc67f9b274ae2f2e343c08fbc31220e84f
Alorian <slowpok19450509@gmail.com>
2023-08-21 18:34:45 +0200

    My first view functions created

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request: HttpRequest) -> HttpResponse:
+    posts = Post.published.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/post/list.html', context=context)
+
+
+def post_detail(request: HttpRequest, id: int) -> HttpResponse:
+    posts = get_object_or_404(Post,
+                              id=id,
+                              status=Post.Status.PUBLISHED)
+
+    return render(request,
+                  'blog/post/detail.html',
+                  {"posts": posts}
+                  )
