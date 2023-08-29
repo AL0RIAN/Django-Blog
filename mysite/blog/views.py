1dec5587c5c0cfbe860271cc85909b7665b5397d
Alorian <slowpok19450509@gmail.com>
2023-08-29 23:39:42 +0200

    My first View Class (PostList)

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,23 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpRequest, HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
 from .models import Post
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request: HttpRequest) -> HttpResponse:
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page_n', 1)
+
     try:
         posts = paginator.page(page_number)
     except PageNotAnInteger:
